607251d46b9d1ed7b0390e6e3f4aabcab6efeddd
VinodKumarDV <dvvinod143@gmail.com>
2023-08-09 15:55:43 +0530

    completed the QR task

---
+++ src/components/QrComp.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function QrComp() {
+  return (
+    <div className="main">
+      <div className="image">
+      <img src="https://i.ibb.co/GdbLtSW/image-qr-code.png" alt="Girl in a jacket" width="250" height="250"/>
+      </div>
+      <div className="text">
+        <h3>Improve your front-end skills by building project</h3>
+        <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+      </div>
+      </div>
+  )
+}
+
+export default QrComp
